74a8a2c51d256d1abf123448ca0be90dc0e7fff3
Berkay Gençdoğan <berkay.gameofbilisim@gmail.com>
2023-07-02 11:58:18 +0300

    Finished

--- src/App.js
+++ src/App.js
@@ -1,10 +1,39 @@
-import './App.css';
-import Courses from './Courses';
+import "bulma/css/bulma.css";
+import "./App.css";
+import Courses from "./Courses";
+import angular from "./images/angular.jpg";
+import bootstrap from "./images/bootstrap5.png";
+import ccsharp from "./images/ccsharp.png";
+import kompleweb from "./images/kompleweb.jpg";
 
 function App() {
+  const desc =
+    "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.";
   return (
     <div className="App">
-      <Courses />
+      <section className="hero is-link">
+        <div className="hero-body">
+          <p className="title">Kurslarım</p>
+        </div>
+      </section>
+      <section className="section">
+        <div className="container">
+          <div className="columns">
+            <div className="column">
+              <Courses title="Angular" desc={desc} img={angular} />
+            </div>
+            <div className="column">
+              <Courses title="Bootstrap" desc={desc} img={bootstrap} />
+            </div>
+            <div className="column">
+              <Courses title="React" desc={desc} img={ccsharp} />
+            </div>
+            <div className="column">
+              <Courses title="Vue" desc={desc} img={kompleweb} />
+            </div>
+          </div>
+        </div>
+      </section>
     </div>
   );
 }
--- src/Courses.js
+++ src/Courses.js
@@ -1,9 +1,24 @@
-import React from 'react'
-
-const Courses = () => {
+import React from "react";
+const Courses = ({ title, desc, img }) => {
   return (
-    <div>Courses</div>
-  )
-}
+    <div>
+      <div className="card">
+        <div className="card-image">
+          <figure className="image is-4by3">
+            <img src={img} alt="" />
+          </figure>
+        </div>
+        <div className="card-content">
+          <div className="media">
+            <div className="media-content">
+              <p className="title is-4">{title}</p>
+            </div>
+          </div>
+          <div className="content">{desc}</div>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default Courses
+export default Courses;
